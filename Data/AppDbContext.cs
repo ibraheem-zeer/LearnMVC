9c085f2463580c9a1de7da4af64523f5367ac7bb
ibraheem Zeer <137792310+ibraheem-zeer@users.noreply.github.com>
2024-09-01 21:14:46 +0300

    CRUD

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LearnMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS01;Database=MVC;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
